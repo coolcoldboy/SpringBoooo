269c28bfd8c03616b04b15aecfc5da655d4a2f89
zhwang <your_registered_github_Email>
2018-07-24 13:36:12 +0800

    logger加入

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.controller;
 
 import com.example.Service.UserService;
 import com.example.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,18 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @RequestMapping("/api/user")
     public User queryUserById(Long id) {
+        logger.info("queryUserById start");
         return userService.findUserById(id);
 }
 
 
     @RequestMapping(value = "/api/updatealbumID", method = {RequestMethod.GET, RequestMethod.POST})
     public int updateTempAlbumID(Long id) {
+        logger.info("updateTempAlbumID start");
         int a = userService.updateTempAlbumID(id);
+        logger.info("updateTempAlbumID end");
         return a;
     }
 }
